605663c5494d5540b11f930fc2a960632ebe2eb7
Alyssa Easter <alyssaeaster@Alyssas-MacBook-Pro.local>
2026-04-07 15:02:41 -0700

    Reconfigured html files

---
+++ lesson4/booking_app_node/public/javascripts/exercise5.js
@@ -0,0 +1,9 @@
+// "Schedules" --> a student can sign up for a schedule
+  // "Please select one schedule" --> drop down of all available schedules (name | date | time)
+  // "Email:" --> text field to enter students email (if email isn't registered to a student, allow entering a new student)
+  // "Submit" button
+
+// "Please provide new student details" --> form to enter new student
+// "Email:" --> text field to enter student email address
+// "Name:" --> text field to enter student name
+// "Booking Sequence:" --> text field to enter student booking sequence number
