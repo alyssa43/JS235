69f86d54760ee0b1e00b9255b7134d28cfe4123e
Alyssa Easter <alyssaeaster@Alyssas-MacBook-Pro.local>
2026-04-08 15:55:28 -0700

    Completeed Book App Part 5

--- lesson4/booking_app_node/public/javascripts/exercise5.js
+++ lesson4/booking_app_node/public/javascripts/exercise5.js
@@ -1,9 +1,126 @@
-// "Schedules" --> a student can sign up for a schedule
-  // "Please select one schedule" --> drop down of all available schedules (name | date | time)
-  // "Email:" --> text field to enter students email (if email isn't registered to a student, allow entering a new student)
-  // "Submit" button
+class StudentCreationUI {
+  constructor(studentEmail, bookingSequence, onSuccess) {
+    this.studentEmail = studentEmail;
+    this.bookingSequence = bookingSequence;
+    this.onSuccess = onSuccess;
+    this.newStudentForm = document.getElementById('newStudentForm');
+    this.emailInput = document.getElementById('email');
+    this.bookingSequenceInput = document.getElementById('booking_sequence');
+    this.#displayPrefilledForm();
+    this.#setupEventListeners();
+  }
 
-// "Please provide new student details" --> form to enter new student
-// "Email:" --> text field to enter student email address
-// "Name:" --> text field to enter student name
-// "Booking Sequence:" --> text field to enter student booking sequence number
+  #setupEventListeners() {
+    this.newStudentForm.addEventListener('submit', event => this.#handleSubmit(event));
+  }
+
+  async #createStudent() {
+    const formData = new FormData(this.newStudentForm);
+    const response = await fetch('/api/students', {
+      method: 'POST',
+      body: formData,
+    });
+
+    return response;
+  }
+
+  async #handleSubmit(event) {
+    event.preventDefault();
+    const response = await this.#createStudent();
+    const responseText = await response.text();
+
+    if (response.ok) {
+      alert(responseText);
+      this.newStudentForm.hidden = true;
+      this.onSuccess();
+    }
+  }
+
+  #displayPrefilledForm() {
+    this.emailInput.value = this.studentEmail;
+    this.bookingSequenceInput.value = this.bookingSequence;
+    this.newStudentForm.hidden = false;
+  }
+}
+
+class StudentBookingUI {
+  constructor() {
+    this.bookingForm = document.getElementById('newBookingForm');
+    this.select = this.bookingForm.querySelector('select');
+    this.submitButton = document.getElementById('submitBtn');
+    this.#addScheduleOptions();
+    this.#setupEventListeners();
+  }
+
+  #setupEventListeners() {
+    this.bookingForm.addEventListener('submit', event => this.#handleSubmit(event));
+  }
+
+  async #handleSubmit(event) {
+    event.preventDefault();
+    await this.#submitBooking();
+  }
+
+  async #submitBooking() {
+      const formData = new FormData(this.bookingForm);
+      const studentEmail = formData.get('student_email');
+      const selectedId = this.select.value;
+      const json = JSON.stringify({ id: selectedId, student_email: studentEmail });
+  
+      const response = await fetch('/api/bookings', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: json,
+      });
+
+      if (response.ok) {
+        alert('Booked');
+        this.bookingForm.reset();
+        await this.#addScheduleOptions();
+      } else if (response.status === 404) {
+        const errorText = await response.text();
+        alert(errorText);
+        const bookingSequence = errorText.match(/\d+/)[0];
+        new StudentCreationUI(studentEmail, bookingSequence, () => this.#submitBooking());
+      }
+  }
+
+  async #addScheduleOptions() {
+    this.select.innerHTML = '';
+    this.submitButton.hidden = true;
+    const staff = await this.#fetchStaff();
+    const available = await this.#fetchAvailableSchedules();
+
+    available.forEach(sch => {
+      const currentStaff = staff.find(s => s.id === sch.staff_id);
+      const option = this.#createScheduleOption(sch, currentStaff);
+      this.select.appendChild(option);
+    });
+
+    this.submitButton.hidden = false;
+  }
+
+  #createScheduleOption(schedule, staff) {
+    const option = document.createElement('option');
+    option.setAttribute('value', schedule.id);
+    option.textContent = `${schedule.id} | ${staff.name} | ${schedule.date} | ${schedule.time}`;
+    return option;
+  }
+  
+  async #fetchSchedules() {
+    const response = await fetch('/api/schedules');
+    return response.json();
+  }
+
+  async #fetchAvailableSchedules() {
+    const schedules = await this.#fetchSchedules();
+    return schedules.filter(sch => !sch.student_email);
+  }
+
+  async #fetchStaff() {
+    const response = await fetch('/api/staff_members');
+    return response.json();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => new StudentBookingUI());
